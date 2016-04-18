ce8b5cf37422e6f7a5113d0f4914f01142e4fbfd
savinos90 <salexa03@cs.ucy.ac.cy>
2016-04-18 13:30:21 +0300

    initialize auxiliary arrays for threads

--- src/main.c
+++ src/main.c
@@ -19,7 +19,6 @@
 #define UPDATE        2 
 
 #define perror2(s, e) fprintf(stderr, "%s:%s\n", s, strerror(e)) 
-#define THREADS 5 
 
 #define RUNNING 1 
 #define WAITING 0 
@@ -31,12 +30,16 @@ int main(int argc, char *argv[]) /* Server with Internet stream sockets */
 {
   // general usage variables
   char temp_string[MAX_STRING];
+  int i = 0;
   
   // variables for reading configuration file
   FILE *config_file = NULL, *users_file = NULL;
   char config_filename[MAX_STRING], users_filename[MAX_STRING];
   int threads_number = 0, port_number = 0;
-  
+
+  // variables for threads
+  int *thread_status = NULL;
+  int *thread_socket = NULL;
   // int port, sock, newsock, serverlen, i; 
   // socklen_t clientlen; 
   // char buf[256]; 
@@ -70,9 +73,9 @@ int main(int argc, char *argv[]) /* Server with Internet stream sockets */
   fscanf(config_file, "%s %d", temp_string, &threads_number);
   fscanf(config_file, "%s %d", temp_string, &port_number);
   fscanf(config_file, "%s %s", temp_string, users_filename);
-
+  
   fclose(config_file);
-
+  
   // ASSUME that users file has a specific stucture
   users_file = fopen(users_filename, "r");
   if(!users_file) {
@@ -80,6 +83,32 @@ int main(int argc, char *argv[]) /* Server with Internet stream sockets */
     exit(1);
   }
   
-    
+  // INITIALIZE common arrays for threads
+  thread_status = (int *) malloc(threads_number * sizeof(int));
+  if(!thread_status) {
+    perror("not enough space");
+    exit(1);
+  } 
+  
+  
+  thread_socket = (int *) malloc(threads_number * sizeof(int));
+  if(!thread_socket) {
+    perror("not enough space");
+    free(thread_status);
+    exit(1);
+  }
+
+  for(i=0; i < threads_number; i++) {
+    thread_status[i] = WAITING;
+    thread_socket[i] = -1;
+  }
+
+  
+  // close files - free memory
+  // TODO clean up in case of exit earlier
+  // TODO check for every exit above
+  free(thread_status);
+  free(thread_socket);
   fclose(users_file);
 }
+
