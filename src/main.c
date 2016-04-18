c9b59e9d3512019b4c53bfbf5f8d1f13653744a9
savinos90 <salexa03@cs.ucy.ac.cy>
2016-04-18 14:00:18 +0300

    initialize and create listen-socket for server

--- src/main.c
+++ src/main.c
@@ -37,14 +37,23 @@ int main(int argc, char *argv[]) /* Server with Internet stream sockets */
   char config_filename[MAX_STRING], users_filename[MAX_STRING];
   int threads_number = 0, port_number = 0;
 
-  // variables for threads
-  int *thread_status = NULL;
-  int *thread_socket = NULL;
+
+  // sockets and server variables
+  int socket_fd = -1, server_sockaddr_len = 0;
+  struct sockaddr_in server_sockaddr; 
+  struct sockaddr *server_sockaddr_ptr;
+  
+
+  // auxiliary threads array
+  int *thread_status_array = NULL, *thread_socket_array = NULL;
+
+    
   // int port, sock, newsock, serverlen, i; 
   // socklen_t clientlen; 
   // char buf[256]; 
-  // struct sockaddr_in server, client; 
-  // struct sockaddr *serverptr, *clientptr; 
+  // struct sockaddr *serverptr, *clientptr;
+  //    struct sockaddr_in server,clint; 
+
   // struct hostent *rem; 
   // pthread_t tid[THREADS]; //threads table 
   // int thread_status[THREADS]; 
@@ -82,33 +91,64 @@ int main(int argc, char *argv[]) /* Server with Internet stream sockets */
     perror("Please initialize a file with users and passwords");
     exit(1);
   }
+
+
+  // CREATE socket for server
+  
+  // function definition http://pubs.opengroup.org/onlinepubs/7908799/xns/socket.html
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    { /* Create socket */
+      perror("socket");
+      exit(1);
+    }
+
+  // initialize server socket
+  server_sockaddr.sin_family = AF_INET; /* Internet domain */
+  server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY); /* My Internet address */
+  server_sockaddr.sin_port = htons(port_number); /* The given port */
+
+  server_sockaddr_ptr = (struct sockaddr *) &server_sockaddr;
+  server_sockaddr_len = sizeof server_sockaddr;
+
+  // function definition of bind  http://pubs.opengroup.org/onlinepubs/7908799/xns/bind.html
+  if (bind(socket_fd, server_sockaddr_ptr, server_sockaddr_len) < 0)
+    { /* Bind socket to an address */
+      perror("bind");
+      exit(1);
+    }
+
+  // function definition http://pubs.opengroup.org/onlinepubs/7908799/xns/listen.html
+  if (listen(socket_fd, threads_number) < 0)
+    { /* Listen for connections */
+      perror("listen");
+      exit(1);
+    }
   
   // INITIALIZE common arrays for threads
-  thread_status = (int *) malloc(threads_number * sizeof(int));
-  if(!thread_status) {
+  thread_status_array = (int *) malloc(threads_number * sizeof(int));
+  if(!thread_status_array) {
     perror("not enough space");
     exit(1);
   } 
-  
-  
-  thread_socket = (int *) malloc(threads_number * sizeof(int));
-  if(!thread_socket) {
+    
+  thread_socket_array = (int *) malloc(threads_number * sizeof(int));
+  if(!thread_socket_array) {
     perror("not enough space");
-    free(thread_status);
+    free(thread_status_array);
     exit(1);
   }
 
   for(i=0; i < threads_number; i++) {
-    thread_status[i] = WAITING;
-    thread_socket[i] = -1;
+    thread_status_array[i] = WAITING;
+    thread_socket_array[i] = -1;
   }
 
   
   // close files - free memory
   // TODO clean up in case of exit earlier
   // TODO check for every exit above
-  free(thread_status);
-  free(thread_socket);
+  free(thread_status_array);
+  free(thread_socket_array);
   fclose(users_file);
 }
 
